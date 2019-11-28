8cbdec139774ca4ecefac2f0820cf479ae0f3958
Joseph Karl Magnussen <44847950+jkmagnussen@users.noreply.github.com>
2019-11-28 13:56:05 +0000

    Add files via upload

---
+++ Build Tools and Single Page Web Apps/src/server/index.js
@@ -0,0 +1,24 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+app.get('/test', function (req, res) {
+    res.json(mockAPIResponse)
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+
